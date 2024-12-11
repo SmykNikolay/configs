ab2047f0cff5cbe8cd48c9537808527ed4410ed0
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-12-11 15:39:09 +0300

    init

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -8,5 +8,10 @@ config.color_scheme = "Gruvbox (Gogh)"
 config.window_decorations = "RESIZE"
 -- config.window_background_opacity = 0.8
 -- config.macos_window_background_blur = 20
-
+config.window_padding = {
+	left = 2,
+	right = 2,
+	top = 0,
+	bottom = 0,
+}
 return config
