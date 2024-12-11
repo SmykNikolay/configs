980f74a2a7c0014ea5a3b612dd672430817c691a
Nikolay Smyk <157750278+SmykNikolay@users.noreply.github.com>
2024-12-11 15:39:19 +0300

    init

--- custom/loops.sh
+++ custom/loops.sh
@@ -8,6 +8,9 @@ SCRIPT_PATH="$SCRIPT_DIR/close_nonwork_apps.sh"
 
 # Выполнение основного скрипта
 while true; do
+  echo "Открытые приложения: "
+  osascript -e 'tell application "System Events" to get name of every application process whose background only is false'
+  echo ""
   "$SCRIPT_PATH"
   sleep 300 # Интервал в 5 минут
 done
